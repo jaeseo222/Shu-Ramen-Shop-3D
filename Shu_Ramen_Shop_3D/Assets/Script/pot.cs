01ae68d63b8f6e3760768225d894bfbd96f8b5ca
jaeserrr@ewhain.net <jaeserrr@ewhain.net>
2022-01-07 08:38:35 +0900

    주전자 이동 & 냄비 물 따르기

--- Shu_Ramen_Shop_3D/Assets/Script/pot.cs
+++ Shu_Ramen_Shop_3D/Assets/Script/pot.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 public class pot : MonoBehaviour
 {
-
-    private void OnMouseUp()
+    WaterPot waterPot;
+    private void Update()
     {
-        //마우스 뗐을 때, 주전자면 물 채워지기
-       // if()
+        waterPot = GameObject.Find("waterPot").GetComponent<WaterPot>();
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetMouseButtonUp(0))
+            switch (other.gameObject.name) {
+                case "waterPot"://물 담기
+                    waterPot.isPot = true;
+                    transform.Find("water").gameObject.SetActive(true);
+                    break;
+            }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        switch (other.gameObject.name)
+        {
+            case "waterPot":
+                waterPot.isPot = false;
+                break;
+        }
+
     }
 }
