5c0d8889276b836ce3882391577c7cd83cd9b57e
jaeseo <jaeserrr@ewhain.net>
2022-07-26 10:26:48 +0900

    [ADD] TimerBar add
    - timer add
    - pot collider fix

---
+++ Assets/Script/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    Image timerBar; // 타이머 색 채움
+    public float maxTime; // 주어진 시간
+    public float currTime; // 현재 시간
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currTime = maxTime;
+        timerBar = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currTime > 0)
+        {
+            currTime -= Time.deltaTime; // 시간 없애기
+            timerBar.fillAmount = currTime / maxTime;
+        }        // 시간 초과되면
+        else
+        {
+            Time.timeScale = 0;
+        }
+    }
+}
